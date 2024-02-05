6c4f48dd18376a4c30b0307c563b94a161670362
Imran <sherlockholmesad221@gmail.com>
2024-02-05 18:24:46 +0600

    initial commit

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,25 +1,57 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
-export default function App(){
-  const step = 1;
-  return <div className="steps">
-    <div className="numbers">
-      <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-      <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-      <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-    </div>
+export default function App() {
+  const [step,setStep] = useState(1);
+  // console.log(step);
 
-    <p className="message">Step {step} : {messages[step-1]}</p>
 
-    <div className="buttons">
-      <button style={{backgroundColor:"#7950f2", color:"#fff"}}>Previous</button>
-      <button style={{backgroundColor:"#7950f2", color:"#fff"}}>Next</button>
+  function handlePrev() {
+    if(step > 1){
+
+      return setStep(step-1);
+    }
+    // setStep(`${step < 0 ? messages.length-1 : step-1}`);
+
+  }
+  function handleNext() {
+    if(step < 3) {
+
+      return setStep(step+1);
+    }
+
+  }
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step} : {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrev}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
     </div>
-  </div>
+  );
 }
-
-
