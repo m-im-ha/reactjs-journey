a676c88a1d3b1d09f03ab1a5fc4a9f0e1fe48e51
Imran <sherlockholmesad221@gmail.com>
2024-02-07 21:36:11 +0600

    initial commit

--- challenge-datecounter/src/App.js
+++ challenge-datecounter/src/App.js
@@ -9,27 +9,46 @@ export default function App() {
 }
 
 function Counter() {
-  const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
+  const [range,setRange] = useState(1);
+  // console.log(+range)
+
+  function handleRange(e){
+    e.preventDefault();
+    setRange(+e.target.value);
+  }
+
+  function handleReset(){
+    setCount(0);
+    setRange(1);
+  }
+
   const date = new Date();
   date.setDate(date.getDate()+count);
-  console.log(date);
+  // console.log(date);
 
   return (
     <div>
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <input type="range" min="0" max="10" value={range} onChange={handleRange}/>
+        <span>Step:{+range}</span>
+        {/* <button onClick={() => setStep((s) => s - 1)}>-</button>
         <span>Step:{step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <button onClick={() => setStep((s) => s + 1)}>+</button> */}
       </div>
       <div>
-        <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Counter:{count}</span>
-        <button onClick={() => setCount((c) => c + step)}>+</button>
+        <button onClick={() => setCount((c) => c - +range)}>-</button>
+        {/* <span>Counter:{count}</span> */}
+        <input type="text" value={count} onChange={(e)=>setCount(Number(e.target.value))}/>
+        <button onClick={() => setCount((c) => c + +range)}>+</button>
       </div>
       <p><span>{count === 0 ? "Today is ":count>0?`${count} day from today is `:`${Math.abs(count)} days ago was `}</span>
       <span>{date.toDateString()}</span>
       </p>
+
+      {(count !== 0 || range !== 1 ) ?<div>
+        <button onClick={handleReset}>Reset</button>
+      </div> : null}
     </div>
   );
 }
