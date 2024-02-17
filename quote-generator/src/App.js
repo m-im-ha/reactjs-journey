3e5d8608f35f3e2385d27ca4fcb4614b36963ca9
Imran <sherlockholmesad221@gmail.com>
2024-02-17 13:04:28 +0600

    initial commit

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -1,25 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [advice, setQuote] = useState("");
+
+  async function getAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setQuote(data.slip.advice);
+  }
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
   return (
-    <div>
+    <div className="app">
       <Title />
-      <Quote />
-      <Button>Quote</Button>
-      <Button>Tweet</Button>
+      <Quote advice={advice} />
+      <Button getAdvice={getAdvice}>Advice</Button>
+      <Button2  advice={advice}>Tweet</Button2>
     </div>
   );
 }
 
 function Title() {
-  return <h1>Quote Generator</h1>;
+  return <h1 className="title">Advice Generator</h1>;
 }
 
-function Quote() {
-  const [quote, setQuote] = useState([]);
-  return <h3>.........................................................</h3>;
+function Quote({ advice }) {
+  return <h3 className="advice">{advice}</h3>;
 }
 
-function Button({ children }) {
-  return <button onClick={() => console.log(`clicked`)}>{children}</button>;
+function Button({ children, getAdvice }) {
+  return <button className="btn" onClick={getAdvice}>{children}</button>;
+}
+
+function Button2({ children,advice }) {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${advice}`;
+
+  return <button className="btn" onClick={()=>window.open(twitterUrl)}>{children}</button>;
 }
