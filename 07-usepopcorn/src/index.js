efbbd155ec86dca3231c315b59c34f167e4005d2
Imran <sherlockholmesad221@gmail.com>
2024-02-19 01:17:31 +0600

    initial commit

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -10,7 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} messages={["Terrible","Bad","Okay","Good","Amazing"]} />
+    <StarRating size={24} color='green' className="test"/>
   </React.StrictMode>
 );
 
