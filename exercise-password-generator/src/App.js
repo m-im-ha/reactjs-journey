9246b033f5407a7c616b0960253559891baa13ae
Imran <sherlockholmesad221@gmail.com>
2024-03-16 12:56:48 +0600

    initial commit

---
+++ exercise-password-generator/src/App.js
@@ -0,0 +1,15 @@
+export default function App(){
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+function Header(){
+  return (
+    <div>
+      <h1>Password Generator</h1>
+    </div>
+  )
+}
