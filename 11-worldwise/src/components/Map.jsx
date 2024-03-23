1977cf49ab3bc86755b65c475c7555d2fb9f877c
Imran <sherlockholmesad221@gmail.com>
2024-03-24 00:36:50 +0600

    initial commit

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,10 +1,20 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 function Map() {
+    const navigator = useNavigate();
+
+    const [searchParams,setSearchParams] = useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+    // console.log(lat,lng);
+
     return (
-        <div className={styles.mapContainer}>
-            Map
+        <div className={styles.mapContainer} onClick={()=>navigator("form")} >
+            <h1>Map</h1>
+            <h1>position : {lat},{lng}</h1>
         </div>
     )
 }
 
 export default Map
+
